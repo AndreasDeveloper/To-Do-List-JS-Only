13bef76b87b750754240faa34cdf16928299847e
AndreasDEV <andreasmrdja16@gmail.com>
2018-07-30 13:16:00 +0200

    Added new functions (Delete). Improved the code. | Update #3 |

--- resources/js/todoapp.js
+++ resources/js/todoapp.js
@@ -2,7 +2,7 @@
 window.setTimeout(function() {
 
 // Array that stores tasks
-var todos = ["Amin"];
+var todos = ["sample"];
 
 // Ask user for a input
 var input = prompt("What would you like to do?");
@@ -10,18 +10,43 @@ var input = prompt("What would you like to do?");
 // Start the loop
 while(input != "quit") {
 if(input === "list") {
-    console.log(todos);
-
+       listTasks();
 } else if(input === "new") {
-        // Ask user to input new task
-        var  newTask = prompt("Enter new task");
-        // Add new task to todos/list array
-        todos.push(newTask);
+       addTask();
+} else if(input === "delete") {
+       deleteTask();
     }
     // Ask for a NEW input
     var input = prompt("What would you like to do?");
 }
 console.log("App Quit");
+
+
+// --- FUNCTIONS --- // 
+function listTasks() {
+    console.log("**********");
+    todos.forEach(function(todo, index) {
+    console.log(index + ": " + todo);
+    });
+    console.log("**********");
+}
+
+function addTask() {
+     // Ask user to input new task
+     var  newTask = prompt("Enter new task");
+     // Add new task to todos/list array
+     todos.push(newTask);
+     console.log("Task added");
+}
+
+function deleteTask() {
+     // Ask for a index of the task to be deleted
+     var index = prompt("Enter the index of the task to delete");
+        
+     // Delete the task
+     todos.splice(index, 1);
+     console.log("Task deleted");
+}
 
 // PART OF THE window.setTiemout function
 }, 500);
